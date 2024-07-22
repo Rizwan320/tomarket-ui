2b02a39cdb9a55b05698fea451fe270ea73cd86b
Rizwan320 <dev.rizwanpir@gmail.com>
2024-07-22 08:30:56 -0700

    implemented buyers from file upload

--- src/layouts/Buyers/index.js
+++ src/layouts/Buyers/index.js
@@ -10,7 +10,11 @@ import buyersdata from "layouts/Buyers/components/data";
 import MDButton from "components/MDButton";
 import api from "../../axios";
 import Loader from "components/Loader";
-import FileUploadButton from "./components/UploadFileButton";
+import UploadFile from "./components/UploadFile";
+import { Modal } from "@mui/material";
+import CsvDownloader from "react-csv-downloader";
+import CloseIcon from "@mui/icons-material/Close";
+import { toast } from "react-toastify";
 
 const COLUMNS = [
   { Header: "Logo", accessor: "logo", align: "left" },
@@ -29,11 +33,86 @@ const COLUMNS = [
   { Header: "Units Sold Last Week", accessor: "unitsSoldLastWeek", align: "center" },
 ];
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 800,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
+const columnss = [
+  {
+    id: "displayName",
+    displayName: "DisplayName",
+  },
+  {
+    id: "email",
+    displayName: "Email",
+  },
+  {
+    id: "city",
+    displayName: "City",
+  },
+  {
+    id: "line1",
+    displayName: "Address",
+  },
+  {
+    id: "postalCode",
+    displayName: "PostalCode",
+  },
+  {
+    id: "countrySubDivisionCode",
+    displayName: "CountrySubDivisioCode",
+  },
+];
+const buyerTemplate = "Download Buyer Template Form Here";
+
+const datas = [
+  // {
+  //   displayName: "Usman",
+  //   email: "usman@tomarket.farm",
+  //   city: "Silicon Valley",
+  //   line1: "Nil",
+  //   postalCode: "100000",
+  //   countrySubDivisionCode: "CA",
+  // },
+];
+
 const Buyers = () => {
   const [loading, setLoading] = useState(false);
   const [tableColumns, setTableColumns] = useState(["logo", "displayName", "email", "showOnMap"]);
   const [refresh, setRefresh] = useState(false);
   const { columns, rows } = buyersdata(tableColumns, refresh);
+  const [open, setOpen] = useState(false);
+  const [file, setFile] = useState();
+
+  const handleOpen = () => setOpen(true);
+
+  const handleModalClose = () => setOpen(false);
+
+  const handleClose = (fileData) => setFile(fileData);
+
+  const handleSubmit = async () => {
+    const formData = new FormData();
+    formData.append("file", file);
+    try {
+      const response = await api.post("file/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      toast.success(response.data.message);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setOpen(false);
+    }
+  };
 
   const refetchBuyers = async () => {
     try {
@@ -66,7 +145,12 @@ const Buyers = () => {
               alignItems: "space-between",
             }}
           >
-            {/* <FileUploadButton /> */}
+            <MDBox mr={2}>
+              <MDButton variant="contained" color="success" onClick={handleOpen}>
+                Upload File
+              </MDButton>
+            </MDBox>
+
             <MDButton
               onClick={() => refetchBuyers()}
               type="button"
@@ -95,6 +179,77 @@ const Buyers = () => {
           />
         </MDBox>
       </Card>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <MDBox sx={style}>
+          <MDBox
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <MDBox
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <MDTypography id="modal-title" variant="h6" component="h2">
+                {buyerTemplate}
+              </MDTypography>
+              <CsvDownloader
+                style={{
+                  backgroundColor: "#4CAF50",
+                  color: "white",
+                  margin: "10px",
+                  padding: 12,
+                  border: "2px solid",
+                  borderRadius: "10px",
+                  fontWeight: "bold",
+                  cursor: "pointer",
+                }}
+                filename="TM-buyer-upload-template"
+                extension=".xlsx"
+                separator=","
+                wrapColumnChar=""
+                columns={columnss}
+                datas={datas}
+                text="DOWNLOAD"
+              />
+            </MDBox>
+
+            <CloseIcon
+              sx={{
+                "&:hover": {
+                  backgroundColor: "rgba(0, 0, 0, 0.1)",
+                  borderRadius: "50%",
+                  cursor: "pointer",
+                },
+              }}
+              onClick={handleModalClose}
+            />
+          </MDBox>
+
+          <UploadFile open={open} onClose={handleClose} />
+          <MDBox
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+              alignItems: "flex-end",
+            }}
+          >
+            <MDButton onClick={handleSubmit} variant="contained" color="success" sx={{ mt: 2 }}>
+              Submit
+            </MDButton>
+          </MDBox>
+        </MDBox>
+      </Modal>
     </>
   );
 };
