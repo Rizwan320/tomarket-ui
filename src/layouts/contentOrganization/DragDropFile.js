943c93924d1904768acf1e45823934b7a3b28e3a
Rizwan320 <dev.rizwanpir@gmail.com>
2024-08-15 04:50:33 -0700

    integrate api for logo and picture

--- src/layouts/contentOrganization/DragDropFile.js
+++ src/layouts/contentOrganization/DragDropFile.js
@@ -18,10 +18,16 @@ import MDButton from "components/MDButton";
 import MDTypography from "components/MDTypography";
 import MDInput from "components/MDInput";
 import FolderIcon from "@mui/icons-material/Folder";
+import api from "../../axios";
+import { toast } from "react-toastify";
+import { useUser } from "context/userContext";
+import Loader from "components/Loader";
 
 const DragDropFile = () => {
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [openDialog, setOpenDialog] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const user = useUser();
 
   const onDrop = useCallback((acceptedFiles) => {
     setUploadedFiles(acceptedFiles);
@@ -31,82 +37,135 @@ const DragDropFile = () => {
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   const folderListItems = ["Product pictures", "Logos", "Sales sheets", "Descriptions"];
+  const listItems = ["pictures", "logo"];
 
   const handleCloseDialog = () => {
     setOpenDialog(false);
   };
 
-  const handleFolderSelection = (folder) => {
-    console.log("Selected folder:", folder);
-    console.log("Uploaded files:", uploadedFiles);
-    handleCloseDialog();
+  const saveImagesToAwsS3Bucket = async (contentType) => {
+    try {
+      setLoading(true);
+      const {
+        user: {
+          user: {
+            account: {
+              accountType: entityType,
+              brand: { id: entityId },
+            },
+          },
+        },
+      } = user;
+
+      const formData = new FormData();
+
+      uploadedFiles.forEach((file) => {
+        formData.append("files", file);
+      });
+      formData.append("contentType", contentType);
+      formData.append("entityType", entityType);
+      formData.append("entityId", entityId);
+      const response = await api.post("content/images", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      if (response?.data?.success) {
+        toast.success(response?.data?.message);
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error?.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
+  const handleFolderSelection = (folder) => {
+    switch (folder) {
+      case folderListItems[0]:
+        saveImagesToAwsS3Bucket(listItems[0]);
+        break;
+      case folderListItems[1]:
+        saveImagesToAwsS3Bucket(listItems[1]);
+        break;
+      case folderListItems[2]:
+        break;
+      case folderListItems[3]:
+        break;
+      default:
+        break;
+    }
+    handleCloseDialog();
+  };
   return (
-    <Container maxWidth="md">
-      <MDTypography variant="h4" gutterBottom>
-        Product upload and organization
-      </MDTypography>
-      <Grid container mt={3} justifyContent="center" alignItems="center">
-        <Grid item xs={12} sm={12} md={7} lg={7}>
-          <MDBox
-            {...getRootProps({ className: "dropzone" })}
-            sx={{
-              border: "2px dashed #aaaaaa",
-              borderRadius: 1,
-              p: 6,
-              mb: 3,
-              textAlign: "center",
-              bgcolor: "background.paper",
-            }}
-          >
-            <MDInput {...getInputProps()} />
-            <CloudUploadIcon color="success" sx={{ fontSize: "5rem" }} />
-            <MDTypography variant="body1">
-              Drag and drop files here, or click to select files
-            </MDTypography>
-            <MDButton color="success" variant="contained" sx={{ mt: 2, color: "white" }}>
-              Browse Files
-            </MDButton>
-          </MDBox>
+    <>
+      {loading && <Loader />}
+      <Container maxWidth="md">
+        <MDTypography variant="h4" gutterBottom>
+          Product upload and organization
+        </MDTypography>
+        <Grid container mt={3} justifyContent="center" alignItems="center">
+          <Grid item xs={12} sm={12} md={7} lg={7}>
+            <MDBox
+              {...getRootProps({ className: "dropzone" })}
+              sx={{
+                border: "2px dashed #aaaaaa",
+                borderRadius: 1,
+                p: 6,
+                mb: 3,
+                textAlign: "center",
+                bgcolor: "background.paper",
+              }}
+            >
+              <MDInput {...getInputProps()} />
+              <CloudUploadIcon color="success" sx={{ fontSize: "5rem" }} />
+              <MDTypography variant="body1">
+                Drag and drop files here, or click to select files
+              </MDTypography>
+              <MDButton color="success" variant="contained" sx={{ mt: 2, color: "white" }}>
+                Browse Files
+              </MDButton>
+            </MDBox>
+          </Grid>
         </Grid>
-      </Grid>
 
-      <Dialog
-        open={openDialog}
-        onClose={handleCloseDialog}
-        sx={{ "& .MuiDialog-paper": { maxWidth: "650px", width: "80%", maxHeight: "90vh" } }}
-      >
-        <DialogTitle>Select a folder to save the file</DialogTitle>
-        <DialogContent>
-          <List>
-            {folderListItems.map((folder) => (
-              <ListItem
-                sx={{
-                  ":hover": {
-                    bgcolor: "#F1F1F1",
-                    cursor: "pointer",
-                  },
-                }}
-                key={folder}
-                onClick={() => handleFolderSelection(folder)}
-              >
-                <MDBox mx={2}>
-                  <FolderIcon />
-                </MDBox>
-                <ListItemText px={2} primary={folder} />
-              </ListItem>
-            ))}
-          </List>
-        </DialogContent>
-        <Divider variant="contained" />
-        <DialogActions>
-          <MDButton color="success" variant="contained" onClick={handleCloseDialog}>
-            Cancel
-          </MDButton>
-        </DialogActions>
-      </Dialog>
-    </Container>
+        <Dialog
+          open={openDialog}
+          onClose={handleCloseDialog}
+          sx={{ "& .MuiDialog-paper": { maxWidth: "650px", width: "80%", maxHeight: "90vh" } }}
+        >
+          <DialogTitle>Select a folder to save the file</DialogTitle>
+          <DialogContent>
+            <List>
+              {folderListItems.map((folder) => (
+                <ListItem
+                  sx={{
+                    ":hover": {
+                      bgcolor: "#F1F1F1",
+                      cursor: "pointer",
+                    },
+                  }}
+                  key={folder}
+                  onClick={() => handleFolderSelection(folder)}
+                >
+                  <MDBox mx={2}>
+                    <FolderIcon />
+                  </MDBox>
+                  <ListItemText px={2} primary={folder} />
+                </ListItem>
+              ))}
+            </List>
+          </DialogContent>
+          <Divider variant="contained" />
+          <DialogActions>
+            <MDButton color="success" variant="contained" onClick={handleCloseDialog}>
+              Cancel
+            </MDButton>
+          </DialogActions>
+        </Dialog>
+      </Container>
+    </>
   );
 };
 
