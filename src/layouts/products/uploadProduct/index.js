39b51e14f85284235b0a3d99f8cb4dd8aba46742
Rizwan320 <dev.rizwanpir@gmail.com>
2024-09-17 23:19:22 +0500

    Added product upload module for TM Admin

--- src/layouts/products/uploadProduct/index.js
+++ src/layouts/products/uploadProduct/index.js
@@ -1,49 +1,22 @@
-import { useState, useRef, useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+import { useState } from "react";
 import { toast } from "react-toastify";
 import Card from "@mui/material/Card";
 
 import api from "../../../axios";
 import Loader from "components/Loader";
-import { Grid } from "@mui/material";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import MDInput from "components/MDInput";
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-import MDButton from "components/MDButton";
+import UploadFile from "layouts/Buyers/components/UploadFile";
 
 const UploadProduct = () => {
   const [loading, setLoading] = useState(false);
   const [file, setFile] = useState(null);
-  const [fileName, setFileName] = useState("");
-  const fileInputRef = useRef(null);
 
-  const onDrop = useCallback((acceptedFiles) => {
-    const selectedFile = acceptedFiles[0];
-    setFile(selectedFile);
-    setFileName(selectedFile ? selectedFile.name : "");
-  }, []);
-
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    multiple: false,
-  });
-
-  const handleFileChange = (event) => {
-    const selectedFile = event.target.files[0];
-    if (selectedFile) {
-      setFile(selectedFile);
-      setFileName(selectedFile.name);
-    }
+  const handleClose = (file) => {
+    setFile(file);
   };
 
-  const handleFileBrowse = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
-
-  const handleSubmit = async () => {
+  const handleSubmit = async (file) => {
     const formData = new FormData();
     formData.append("file", file);
 
@@ -72,53 +45,11 @@ const UploadProduct = () => {
             Products
           </MDTypography>
         </MDBox>
-        <Grid container mt={3} padding={10} justifyContent="center" alignItems="center">
-          <Grid item xs={12} sm={12} md={9} lg={9}>
-            <MDBox
-              {...getRootProps({ className: "dropzone" })}
-              display="flex"
-              flexDirection="column"
-              alignItems="center"
-              justifyContent="center"
-              sx={{
-                border: "2px dashed #aaaaaa",
-                borderRadius: 1,
-                p: 8,
-                mb: 3,
-                textAlign: "center",
-                bgcolor: "background.paper",
-              }}
-            >
-              <MDInput {...getInputProps()} />
-              {fileName && <MDTypography variant="body1">{fileName}</MDTypography>}
-
-              <CloudUploadIcon color="success" sx={{ fontSize: "5rem" }} />
-              <MDTypography variant="body1">
-                Drag and drop files here, or click to select files
-              </MDTypography>
-              <MDInput
-                ref={fileInputRef}
-                type="file"
-                style={{ display: "none" }}
-                onChange={handleFileChange}
-              />
-
-              <MDButton
-                color="success"
-                variant="contained"
-                onClick={handleFileBrowse}
-                sx={{ mt: 2, color: "white" }}
-              >
-                Browse Files
-              </MDButton>
-            </MDBox>
-            <MDBox display="flex" justifyContent="center" mt={3}>
-              <MDButton onClick={handleSubmit} variant="contained" color="success">
-                Submit
-              </MDButton>
-            </MDBox>
-          </Grid>
-        </Grid>
+        <UploadFile
+          onClose={handleClose}
+          submitButton={true}
+          handleSubmit={() => handleSubmit(file)}
+        />
       </Card>
     </>
   );
