aa9edb6f86153ba7f3154e2359b1f542951f1a82
Rizwan320 <dev.rizwanpir@gmail.com>
2024-09-24 20:43:50 +0500

    Updated Brand Table on admin side

--- src/layouts/superAdmin/brands/data/index.js
+++ src/layouts/superAdmin/brands/data/index.js
@@ -10,12 +10,18 @@ export const tableAccountData = (accounts) => {
   return {
     columns: [
       { Header: "Brand Name", accessor: "brandName", align: "left" },
-      { Header: "Status", accessor: "status", align: "center" },
+      { Header: "# of Connected Distributors", accessor: "connectionCount", align: "center" },
+      { Header: "Total Products", accessor: "productCount", align: "center" },
+      { Header: "Contact Name", accessor: "userName", align: "left" },
+      { Header: "Contact Email", accessor: "email", align: "left" },
     ],
     rows: accounts?.map((account) => ({
-      id: account?.id,
-      brandName: <Name name={account?.accountName} />,
-      status: <Name name={account?.status} />,
+      id: account?.accountid,
+      brandName: <Name name={account?.brandname || "N/A"} />,
+      connectionCount: <Name name={account?.connectioncount || 0} />,
+      productCount: <Name name={account?.productcount || 0} />,
+      userName: <Name name={account?.username || "N/A"} />,
+      email: <Name name={account?.email || "N/A"} />,
     })),
   };
 };
