6aacb7c3fa042be91dbe2bf43eeefd0373975a98
Rizwan320 <dev.rizwanpir@gmail.com>
2024-10-04 19:15:15 +0500

    Added Payment Details Component on Brand Side

--- src/layouts/billing/components/PaymentDetails/index.js
+++ src/layouts/billing/components/PaymentDetails/index.js
@@ -1,76 +1,94 @@
-import React, { useEffect, useState } from "react";
-import { Grid, Card, Chip } from "@mui/material";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+
+import { Grid, Card, Chip } from "@mui/material";
+
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
+import Loader from "components/Loader";
+
 import MasterCard from "../MasterCard";
 import PaymentDialog from "../PaymentDialog";
+
 import api from "../../../../axios";
 
 const PaymentDetails = () => {
-  const [payemntDetails, setPaymentDetails] = useState();
+  const [paymentDetails, setPaymentDetails] = useState();
   const [open, setOpen] = useState();
+  const [isActive, setIsActive] = useState("inactive");
+  const [loading, setLoading] = useState(false);
 
   const handleOnSubmit = async (values) => {
     try {
-      const { data: response } = await api.post("payment/details", values);
-      setPaymentDetails(response);
+      const { data } = await api.post("payment/details", values);
+      setPaymentDetails(data);
       setOpen(false);
     } catch (error) {
-      toast.error(error.response.data.message);
+      toast.error(error?.response?.data?.message);
     }
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true);
         const { data: response } = await api.get("payment/details");
         setPaymentDetails(response);
+        response?.last4 ? setIsActive("active") : setIsActive("inactive");
       } catch (error) {
-        console.error("Error:", error.response.data.message);
+        setIsActive("inactive");
+      } finally {
+        setLoading(false);
       }
     };
     fetchData();
   }, []);
+
   return (
     <>
-      {payemntDetails && (
-        <Card sx={{ my: 2, p: 2 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <MDTypography variant="h6" mb={2}>
-                Payment Details
-              </MDTypography>
-              <MDBox maxWidth="450px">
-                <MasterCard
-                  number={`************${payemntDetails.last4}`}
-                  holder={payemntDetails.name}
-                  expires={`${payemntDetails.expiry_month}/${payemntDetails.expiry_year
-                    .toString()
-                    .slice(2, 4)}`}
-                  cardType={payemntDetails.card_type}
-                  onEdit={() => setOpen(true)}
-                />
-              </MDBox>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <MDTypography variant="h6" mb={1}>
-                Subscription Details
-              </MDTypography>
-              <MDBox display="flex" gap={2}>
+      {loading && <Loader />}
+      <Card sx={{ my: 2, p: 2, mb: 10 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <MDTypography variant="h6" mb={2}>
+              Payment Details
+            </MDTypography>
+            <MDBox maxWidth="450px">
+              <MasterCard
+                number={`************${paymentDetails?.last4 ?? "****"}`}
+                holder={paymentDetails?.name || ""}
+                expires={`${paymentDetails?.expiry_month || ""}/${
+                  paymentDetails?.expiry_year?.toString()?.slice(2, 4) || ""
+                }`}
+                cardType={paymentDetails?.card_type}
+                onEdit={isActive === "active" ? () => setOpen(true) : null}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <MDTypography variant="h6" mb={1}>
+              Subscription Details
+            </MDTypography>
+            <MDBox>
+              <Chip
+                label={isActive}
+                variant="outlined"
+                color={isActive === "active" ? "success" : "error"}
+              />
+            </MDBox>
+            {/* <MDBox display="flex" gap={2}>
                 <MDTypography mb={1}>
-                  {`${payemntDetails.subscription.frequency} ${payemntDetails.subscription.amount}$`}
+                  {`${paymentDetails.subscription.frequency} ${paymentDetails.subscription.amount}$`}
                 </MDTypography>
                 <Chip
-                  label={payemntDetails.subscription.status}
+                  label={paymentDetails.subscription.status}
                   variant="outlined"
-                  color={payemntDetails.subscription.status === "active" ? "success" : "error"}
+                  color={paymentDetails.subscription.status === "active" ? "success" : "error"}
                 />
-              </MDBox>
-            </Grid>
+              </MDBox> */}
           </Grid>
-        </Card>
-      )}
+        </Grid>
+      </Card>
       {open && (
         <PaymentDialog
           title={
--- src/layouts/dashboard/components/AddPaymnetAlert/index.js
+++ src/layouts/dashboard/components/AddPaymnetAlert/index.js
@@ -1,32 +1,20 @@
-import React, { useState } from "react";
 import MDTypography from "components/MDTypography";
 import MDButton from "components/MDButton";
-import { useUser } from "context/userContext";
 import MDAlert from "components/MDAlert";
 import MDBox from "components/MDBox";
 
-const AddPaymnetAlert = ({ onClick, trialStartDate }) => {
-  let trialEndDate;
-  if (trialStartDate) {
-    trialEndDate = new Date(trialStartDate);
-    trialEndDate.setDate(trialEndDate.getDate() + 30);
-  }
-
-  return (
-    <MDAlert color="secondary" dismissible>
-      <MDBox display="flex" justifyContent="center" alignItems="center" gap={2}>
-        <MDTypography variant="h6" color="light">
-          🌟 Heads up! You are currently enjoying a free trial
-          {trialEndDate ? ` until ${trialEndDate.toLocaleDateString()}` : ""}. Make sure to add your
-          payment details
-          <MDButton onClick={onClick} color="success" size="small" variant="text">
-            here
-          </MDButton>
-          {"to continue."}
-        </MDTypography>
-      </MDBox>
-    </MDAlert>
-  );
-};
+const AddPaymnetAlert = ({ onClick }) => (
+  <MDAlert color="secondary" dismissible>
+    <MDBox display="flex" justifyContent="center" alignItems="center" gap={2}>
+      <MDTypography variant="h6" color="light">
+        🌟 Heads up! Make sure to add your payment details
+        <MDButton onClick={onClick} color="success" size="small" variant="text">
+          here
+        </MDButton>
+        {"to continue."}
+      </MDTypography>
+    </MDBox>
+  </MDAlert>
+);
 
 export default AddPaymnetAlert;
