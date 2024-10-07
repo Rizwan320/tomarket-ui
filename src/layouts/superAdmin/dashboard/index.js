367b6eff1dcfc9a6687c0b9b7ecc935705a4d8c6
Rizwan320 <dev.rizwanpir@gmail.com>
2024-10-08 01:00:34 +0500

    implemented buyers map on admin side

--- src/layouts/superAdmin/dashboard/index.js
+++ src/layouts/superAdmin/dashboard/index.js
@@ -2,20 +2,24 @@ import { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 
 import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
 
 import MDBox from "components/MDBox";
 import InfoCard from "muiComponents/Cards/InfoCards/InfoCard";
 
 import api from "../../../axios";
 import ChangePasswordModal from "../../Modals/ChangePasswordModal.js";
+import Maps from "google/Map";
 
 const SuperAdminDashboard = () => {
   const [totalDistributors, setTotalDistributors] = useState(0);
   const [totalBrands, setTotalBrands] = useState(0);
+  const [updatedMarker, setUpdatedMarker] = useState([]);
 
   useEffect(() => {
     fetchBrands();
     fetchDistributors();
+    fetchBuyersLocation();
   }, []);
 
   const fetchBrands = async () => {
@@ -31,6 +35,14 @@ const SuperAdminDashboard = () => {
     try {
       const distributorsResponse = await api.get("distributors");
       setTotalDistributors(distributorsResponse?.data?.length);
+    } catch (error) {
+      toast.error(error?.response?.data?.message || error?.message);
+    }
+  };
+  const fetchBuyersLocation = async () => {
+    try {
+      const res = await api.get("admin/locations");
+      setUpdatedMarker(res?.data);
     } catch (error) {
       toast.error(error?.response?.data?.message || error?.message);
     }
@@ -56,6 +68,18 @@ const SuperAdminDashboard = () => {
           </Grid>
         ))}
       </Grid>
+      <MDBox mt={4.5}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={8}>
+            <Card>
+              <MDBox p={2}>
+                <Maps data={updatedMarker} />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
+
       <ChangePasswordModal />
     </MDBox>
   );
